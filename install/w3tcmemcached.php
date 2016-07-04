dc502f30b0da860d37960a2a0ed672561923749a
Julio Sene França <julio.sene@gmail.com>
2016-07-04 11:21:01 -0300

    Update w3tcmemcached.php

--- install/w3tcmemcached.php
+++ install/w3tcmemcached.php
@@ -1,20 +1,29 @@
 <?php
 $azuretest = include 'master.php';
-print_r($azuretest);
+// echo("<pre>");
+// print_r($azuretest);
+// echo("</pre>");
 
-$azuretest['objectcache.memcached.servers'] = array("'127.0.0.1:' . getenv('MEMCACHESHIM_PORT')");
-$azuretest['dbcache.memcached.servers'] = array("'127.0.0.1:' . getenv('MEMCACHESHIM_PORT')");
-$azuretest['pgcache.memcached.servers'] = array("'127.0.0.1:' . getenv('MEMCACHESHIM_PORT')");
-$azuretest['minify.memcached.servers'] = array("'127.0.0.1:' . getenv('MEMCACHESHIM_PORT')");
-$azuretest['fragmentcache.memcached.servers'] = array("'127.0.0.1:' . getenv('MEMCACHESHIM_PORT')");
+if($azuretest['dbcache.engine']=='memcached')
+    $azuretest['dbcache.memcached.servers'] = array("%IP_MEMCACHESHIM_PORT%");
+if ($azuretest['pgcache.memcached.servers'])
+    $azuretest['pgcache.memcached.servers'] = array("%IP_MEMCACHESHIM_PORT%");
+if ($azuretest['minify.memcached.servers'])
+    $azuretest['minify.memcached.servers'] = array("%IP_MEMCACHESHIM_PORT%");
+if($azuretest['fragmentcache.memcached.servers'])
+    $azuretest['fragmentcache.memcached.servers'] = array("%IP_MEMCACHESHIM_PORT%");
+if ($azuretest['objectcache.memcached.servers'])
+    $azuretest['objectcache.memcached.servers'] = array("%IP_MEMCACHESHIM_PORT%");
+
+
+$azuretest = var_export($azuretest, TRUE);
+$azuretest = str_replace("'%IP_MEMCACHESHIM_PORT%'", "'127.0.0.1:' . getenv(\"MEMCACHESHIM_PORT\")", $azuretest);
 
 $newfile = fopen("master.php", "w+");
 
-fwrite($newfile,"<?php\n\nretun ");
-fwrite($newfile,var_export($azuretest, TRUE));
-fwrite($newfile,"\n?>");
+fwrite($newfile,"<?php\n\nreturn ");
+fwrite($newfile,$azuretest);
+
+fwrite($newfile,';');
 fclose($newfile);
-//print_r ($azuretest['objectcache.memcached.servers']);
-//echo ("<br/><br/>");
-//var_dump ($azureMencached);
 ?>

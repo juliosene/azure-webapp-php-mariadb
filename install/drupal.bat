7518036d6846a6f33fc48c763876cfe601c7e372
Julio Sene França <julio.sene@gmail.com>
2016-08-16 13:49:17 -0300

    Update drupal.bat

--- install/drupal.bat
+++ install/drupal.bat
@@ -2,6 +2,5 @@ cd \home\site
 echo $client = new-object System.Net.WebClient > download.ps1
 echo $client.DownloadFile("https://ftp.drupal.org/files/projects/drupal-8.1.8.zip","drupal.zip") >> download.ps1
 powershell -file download.ps1
-unzip ..\drupal.zip 
 cd wwwroot
 unzip ..\drupal.zip
